9f0e1798a9624bacbd26eee997e3726f292c1298
coconut-cracker <60694070+coconut-cracker@users.noreply.github.com>
2020-03-10 14:29:19 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+document.querySelector(".get-jokes").addEventListener("click", getJokes);
+
+function getJokes(e) {
+  e.preventDefault();
+
+  const number = document.querySelector("input[type = number]").value;
+  console.log(number);
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
+
+      let output = [];
+
+      if (response.type === "success") {
+        response.value.forEach(function(joke) {
+          output += `<li>${joke.joke}</li>`;
+        });
+      } else {
+        output += "<li>Something went wrong</li>";
+      }
+
+      document.querySelector(".jokes").innerHTML = output;
+      console.log(response);
+    }
+  };
+
+  xhr.send();
+}
